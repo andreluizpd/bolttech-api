98794e98192a6c9bf8138986a3d2be18c0acabad
André Luiz Pastore Dambros <andreluizpd@gmail.com>
2021-07-03 14:44:09 -0300

    Feat: Added user authentication

---
+++ config/default.example.ts
@@ -0,0 +1,9 @@
+export default {
+  port: 1337,
+  host: 'localhost',
+  dbUri: 'Mongodb URL',
+  saltWorkFactor: 10,
+  accessTokenTtl: '15m',
+  refreshTokenTtl: '1y',
+  privateKey: `Private key to generate JWTs`,
+};
---
+++ src/routes.ts
@@ -0,0 +1,33 @@
+import { Express, Request, Response } from 'express';
+
+import { createUserHandler } from './controller/user.controller';
+import {
+  createUserSessionHandler,
+  invalidateUserSessionHandler,
+  getUserSessionsHandler,
+} from './controller/session.controller';
+import { validateRequest, requiresUser } from './middleware';
+import {
+  createUserSchema,
+  createUserSessionSchema,
+} from './schema/user.schema';
+
+export default function (app: Express) {
+  app.get('/healthcheck', (req: Request, res: Response) => res.sendStatus(200));
+
+  // Register User
+  app.post('/api/users', validateRequest(createUserSchema), createUserHandler);
+
+  // Login
+  app.post(
+    '/api/sessions',
+    validateRequest(createUserSessionSchema),
+    createUserSessionHandler
+  );
+
+  // Get user's sessions
+  app.get('/api/sessions', requiresUser, getUserSessionsHandler);
+
+  // Logout
+  app.delete('/api/sessions', requiresUser, invalidateUserSessionHandler);
+}
